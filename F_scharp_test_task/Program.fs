79fd036267c3eca8f696ed63ab738f0c5fe5f7a1
Kazanin Roman <krp@sib-net.ru>
2024-02-20 11:50:27 +0600

    inital

---
+++ F_scharp_test_task/Program.fs
@@ -0,0 +1,50 @@
+﻿open System
+open Akka.Actor
+open System.Net.Http
+
+type HttpRequestMessage(url: string, method: string) =
+    member val Url = url
+    member val Method = method
+
+type LogResponse(response: string) =
+    member val Response = response
+
+
+type HttpRequestActor(responseLoggerActor: IActorRef) =
+    inherit UntypedActor()
+
+    let httpClient = new HttpClient()
+
+    override this.OnReceive message =
+        match message with
+        | :? HttpRequestMessage as msg ->
+            try
+                let response = Async.RunSynchronously(Async.AwaitTask(httpClient.GetAsync(msg.Url)))
+                let responseBody = response.Content.ReadAsStringAsync().Result
+
+                responseLoggerActor.Tell(LogResponse(responseBody))
+            with
+            | ex -> Console.WriteLine(ex.Message)
+        | _ -> ()
+
+type ResponseLoggerActor() =
+    inherit UntypedActor()
+
+    override this.OnReceive message =
+        match message with
+        | :? LogResponse as msg ->
+            Console.WriteLine(msg.Response)
+        | _ -> ()
+
+//Begin
+let system = ActorSystem.Create("MySystem")
+
+let responseLoggerActor = system.ActorOf(Props.Create(fun () -> ResponseLoggerActor()), "responseLoggerActor")
+let httpRequestActor = system.ActorOf(Props.Create(fun () -> HttpRequestActor(responseLoggerActor)), "httpRequestActor")
+
+httpRequestActor.Tell(HttpRequestMessage("https://jsonplaceholder.typicode.com/posts/10", "GET"), responseLoggerActor)
+httpRequestActor.Tell(HttpRequestMessage("https://jsonplaceholder.typicode.com/posts/1", "GET"), responseLoggerActor)
+
+Console.WriteLine "Демонстрация асинхронности"
+
+Console.ReadLine() |> ignore
